fc661517bf8ae4e4dbe50dc8333a603bd16d17c9
Ryan-Lucey-13 <rlucey1300@gmail.com>
2025-01-13 14:01:01 -0500

    edits

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -80,9 +80,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgres://localhost/dbname'
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
